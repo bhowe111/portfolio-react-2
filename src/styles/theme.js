6aecb6f0e1312142e97b9a8e1e2d8b1ec7d27b14
bhowe111 <brendan.howe1@gmail.com>
2021-01-10 15:02:26 -0800

    transfered styling into theme and global from css files, to move toggle into header

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,26 @@
+import whiteBkgImage from "../images/ams-bkg-blur-color-grit.jpg";
+import goldBkgImage from "../images/ams-bkg-blur-dark-hatch.png";
+
 export const whiteTheme = {
-  body: "#e3e3e3",
-  text: "#fff",
   toggleBorder: "slateblue",
   gradient: "linear-gradient(#39598a, #79d7ed)",
+  mainText: "#04e0e0",
+  secondaryText: "#c3c3c3",
+  backgroundImage: `url(${whiteBkgImage})`,
+  mainColor: "#000",
+  secondaryColor: "#fff",
+  borderColor: "#02a164",
+  previewShadow: "#00bdcaa1",
 };
 
 export const goldTheme = {
-  body: "#888",
-  text: "gold",
   toggleBorder: "gold",
   gradient: "linear-gradient(#091236, #1e215d)",
+  mainText: "#ffd700",
+  secondaryText: "#fff",
+  backgroundImage: `url(${goldBkgImage})`,
+  mainColor: "#161616",
+  secondaryColor: "#303030",
+  borderColor: "#c6c6c6",
+  previewShadow: "#757575a1",
 };
